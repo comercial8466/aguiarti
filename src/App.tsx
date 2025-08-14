c6c15bd90ef740ce743f149ecb2ad5a550aa8440
Tarciso102030 <tarciso@aguiarti.com.br>
2025-08-14 16:04:30 -0300

    Add AguiarT.I logo to header

--- src/App.tsx
+++ src/App.tsx
@@ -57,8 +57,11 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4">
             <div className="flex items-center space-x-2">
-              <Computer className="h-8 w-8 text-blue-600" />
-              <span className="text-2xl font-bold text-gray-900">AguiarT.I</span>
+              <img 
+                src="/LOGO.png" 
+                alt="AguiarT.I Logo" 
+                className="h-10 w-auto"
+              />
             </div>
             
             {/* Desktop Navigation */}
