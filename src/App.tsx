41d154c0010dc6959214adc3a295c391abfa39af
Tarciso102030 <tarciso@aguiarti.com.br>
2025-10-17 19:54:01 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -287,7 +287,7 @@ function App() {
                   Venda de Notebook, Computadores e Servidores
                  </li>
                 <li className="flex items-center gap-2">
-                  <CheckCircle className="h-6 w-6 text-green-500" />
+                  <CheckCircle className="h-9 w-9 text-green-500" />
                   Venda e implantação de Switches e Roteadores, como Mikrotik e outros                       fabricantes líderes no mercado
                   </li>
                 <li className="flex items-center gap-2">
