a20b41fc6cdf199bc525f19db9a226d8027858b1
Tarciso102030 <tarciso@aguiarti.com.br>
2025-10-17 17:04:15 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -283,7 +283,7 @@ function App() {
                   Backup e segurança
                 </li>
                  <li className="flex items-center gap-2">
-                  <CheckCircle className="h-10 w-10 text-green-500" />
+                  <CheckCircle className="h-5 w-5 text-green-500" />
                   Oferecemos a venda de uma ampla gama de equipamentos de TI, incluindo:
 Computadores e Servidores: Soluções completas para ambientes de trabalho e data centers.
 Switches: Modelos gerenciáveis e não gerenciáveis, adequados para redes de todos os tamanhos.
