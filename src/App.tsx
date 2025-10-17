8c45439517d5e6808a580cf6d70fc46f630fd56d
Tarciso102030 <tarciso@aguiarti.com.br>
2025-10-17 20:35:47 -0300

    Ajustar tamanho do ícone CheckCircle para consistência

--- src/App.tsx
+++ src/App.tsx
@@ -283,7 +283,7 @@ function App() {
                   Backup e segurança
                 </li>
                  <li className="flex items-center gap-2">
-                  <CheckCircle className="h-6 w-6 text-green-500" />
+                  <CheckCircle className="h-5 w-5 text-green-500" />
                   Venda de Notebook, Computadores e Servidores
                  </li>
                 <li className="flex items-center gap-2">
