dcc5da5142246afb87d4e56c55367d6450383627
Tarciso102030 <tarciso@aguiarti.com.br>
2025-10-22 12:03:56 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -254,7 +254,7 @@ function App() {
                  Implantação e Configurações de redes de Computadores
               </li>
                 <li className="flex items-center gap-2">
-                  <CheckCircle className="h-5 w-5 text-green-500" />
+                  <CheckCircle className="h-7 w-7 text-green-500" />
                   Monitoramento de Redes, Desktop e Servidores 
               </li>
                 <li className="flex items-center gap-2">
