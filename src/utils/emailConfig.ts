fafd97147da09bb3da06c97317b5b9db4167f35b
comercial8466 <comercial@aguiarti.com.br>
2025-10-31 21:31:19 -0300

    Updated emailConfig.ts

--- src/utils/emailConfig.ts
+++ src/utils/emailConfig.ts
@@ -1,38 +1,6 @@
-// Configurações do EmailJS - CONFIGURADO
 export const EMAIL_CONFIG = {
-  // Service ID do EmailJS
   SERVICE_ID: 'service_7hd9nhk',
-  
-  // Template ID do EmailJS - ATUALIZADO
   TEMPLATE_ID: 'template_3o1yljj',
-  
-  // Public Key do EmailJS
   PUBLIC_KEY: 'vpNB9cX2dzVmSvLpk',
-  
-  // E-mail de destino (onde as mensagens serão recebidas)
   TO_EMAIL: 'tarciso@aguiarti.com.br'
 };
-
-// Template de e-mail configurado no EmailJS:
-/*
-Assunto: Nova mensagem do site AguiarT.I - {{from_name}}
-
-Olá,
-
-Você recebeu uma nova mensagem através do formulário de contato do site AguiarT.I:
-
-📋 DADOS DO CONTATO:
-Nome: {{from_name}}
-E-mail: {{from_email}}
-Empresa: {{company}}
-Telefone: {{phone}}
-Serviço de Interesse: {{service}}
-
-💬 MENSAGEM:
-{{message}}
-
----
-Para responder, basta reply este e-mail ou entre em contato diretamente com {{from_email}}.
-
-Esta mensagem foi enviada automaticamente através do formulário de contato do site AguiarT.I.
-*/
